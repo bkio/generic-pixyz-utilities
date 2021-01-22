8bea1dc4d2b60fa0d053e754be99032636817278
Hannes De Jager <jdj.hans@gmail.com>
2021-01-22 10:50:34 +0100

    Messages changed to compressed base 64 strings.
    Added ability to send lods of geometry parts over separate messages

---
+++ services/PixyzWorkerProcess/Processing/Models/LodMessage.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using ServiceUtilities.Process.RandomAccessFile;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixyzWorkerProcess.Processing.Models
+{
+    public class LodMessage:GeometryNode
+    {
+        public const string LODS_PROPERTY = "lodNumber";
+        [JsonProperty(LODS_PROPERTY)]
+        public int LodNumber { get; set; }
+    }
+}
